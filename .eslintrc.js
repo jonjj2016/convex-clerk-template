e28f956f7652f2e096dae91e6c7e543f71f0a66e
Jon <jonmartirosyan1988@gmail.com>
2025-03-02 03:10:27 +0800

    some providers like convex theme and toster basik things

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,37 @@
-module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
-    ],
-    parser: '@babel/eslint-parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['react', 'prettier'],
-    rules: {
-        'prettier/prettier': 'error',
-        // Add any other custom rules here
-    },
-}; 
+// module.exports = {
+//     env: {
+//         browser: true,
+//         es2021: true,
+//     },
+//     extends: [
+//         'eslint:recommended',
+//         'plugin:react/recommended',
+//         'plugin:prettier/recommended',
+//     ],
+//     parser: '@babel/eslint-parser',
+//     parserOptions: {
+//         ecmaFeatures: {
+//             jsx: true,
+//         },
+//         ecmaVersion: 12,
+//         sourceType: 'module',
+//     },
+//     plugins: ['react', 'prettier'],
+//     rules: {
+//         'prettier/prettier': 'error',
+//         // Add any other custom rules here
+//     },
+// };
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+];
