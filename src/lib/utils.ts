ad6a2d1a6160e07f699d24f30cea2b7cc893d430
Jon <jonmartirosyan1988@gmail.com>
2025-03-02 16:02:25 +0800

    added some utils and schemas

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,31 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
+
+export const createSlugsFromName = (title: string) => {
+    return title
+        .toLowerCase()
+        .replace(/[^a-z0-9]+/g, '_')
+        .replace(/(^-|-$)+/g, '');
+};
+export const combineName = (
+    user: {
+        firstName?: string;
+        lastName?: string;
+    } | null
+) => {
+    if (!user) return 'Anonymous';
+    if (!user.firstName && !user.lastName) return 'Anonymous';
+    return `${user.firstName} ${user.lastName}`;
+};
+export const formatDate = (date: number) => {
+    const formatter = new Intl.DateTimeFormat('en', {
+        month: 'short',
+        day: 'numeric',
+        year: 'numeric',
+    });
+    return formatter.format(date);
+};
